25918130365e2a7af5a3fe287c96c74165d8925c
Leo Liu <zmliu0077@gmail.com>
2022-03-07 23:50:01 +0800

    添加单元测试

--- w2_2_code/test/Score.test.js
+++ w2_2_code/test/Score.test.js
@@ -21,19 +21,19 @@ describe('Score', function () {
     teacher = await Teacher.deploy(score.address)
 
     // 设置wallet1为teacher
-    await score.setTeacher(wallet.address)
+    await score.setTeacher(teacher.address)
   })
 
   it('only teacher can modify score', async function () {
-    await expect(score.connect(wallet2).setScore(wallet3.address, 99)).to.be.revertedWith('Score: NOT_TEACHER')
+    await expect(score.setScore(wallet3.address, 99)).to.be.revertedWith('Score: NOT_TEACHER')
   })
 
   it('score must be below 100', async function () {
-    await expect(score.setScore(wallet3.address, 101)).to.be.revertedWith('Score: INVALID_SCORE')
+    await expect(teacher.setScore(wallet3.address, 101)).to.be.revertedWith('Score: INVALID_SCORE')
   })
 
   it('score must be set correct', async function () {
-    await score.setScore(wallet3.address, 98)
+    await teacher.setScore(wallet3.address, 98)
     expect(await score.studentScore(wallet3.address)).to.be.equal(98)
   })
 })
